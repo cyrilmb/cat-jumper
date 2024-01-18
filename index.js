3e582fdf155e8d0f4bdfe7802eca6c6fb4479d9c
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-18 17:24:08 -0600

    draw and update in Player class

--- index.js
+++ index.js
@@ -4,16 +4,43 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
-let y = 100;
-let x = 100;
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+    this.sides = {
+      bottom: this.position.y + this.height,
+    };
+  }
+
+  draw() {
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    if (this.sides.bottom < canvas.height) {
+      this.position.y++;
+      this.sides.bottom = this.position.y + this.height;
+    }
+  }
+}
+
+const player = new Player();
+
+// let bottom = y + 100;
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = 'grey';
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  c.fillStyle = 'red';
-  c.fillRect(x, y, 100, 100);
-  y++;
+  player.draw();
+  player.update();
 }
 
 animate();
