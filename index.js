8dd7bb0f650f9921c773b69bc42657fd2ee2e39c
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-18 17:53:34 -0600

    left and right movement

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ canvas.height = 64 * 9;
 
 const player = new Player();
 
-// let bottom = y + 100;
-
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = 'grey';
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
 
   player.draw();
   player.update();
@@ -23,7 +36,25 @@ window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'w':
       if (player.velocity.y === 0) {
-        player.velocity.y = -10;
+        player.velocity.y = -20;
       }
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      break;
+    case 'd':
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 'd':
+      keys.d.pressed = false;
+      break;
   }
 });
