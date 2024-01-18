182b62e9b1b6373b96c1a2b26f0359b48536093b
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-18 17:33:31 -0600

    simple gravity

--- index.js
+++ index.js
@@ -4,32 +4,6 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
-class Player {
-  constructor() {
-    this.position = {
-      x: 100,
-      y: 100,
-    };
-    this.width = 100;
-    this.height = 100;
-    this.sides = {
-      bottom: this.position.y + this.height,
-    };
-  }
-
-  draw() {
-    c.fillStyle = 'red';
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y++;
-      this.sides.bottom = this.position.y + this.height;
-    }
-  }
-}
-
 const player = new Player();
 
 // let bottom = y + 100;
