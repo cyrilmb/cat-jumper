48e480aad9cf8f5e0ba597fd54d43568fbf50278
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-18 18:25:53 -0600

    found a bookshelf png, but the dimensions are wrong... not sure if I should adjust the canvas or get a new image

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+class Sprite {
+  constructor({ position }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = './img/bookshelf.png';
+  }
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+
+const backgroundLevel1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+});
+
 const player = new Player();
 
 const keys = {
@@ -19,8 +37,8 @@ const keys = {
 };
 function animate() {
   window.requestAnimationFrame(animate);
-  c.fillStyle = 'grey';
-  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  backgroundLevel1.draw();
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
@@ -31,30 +49,3 @@ function animate() {
 }
 
 animate();
-
-window.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'w':
-      if (player.velocity.y === 0) {
-        player.velocity.y = -20;
-      }
-      break;
-    case 'a':
-      keys.a.pressed = true;
-      break;
-    case 'd':
-      keys.d.pressed = true;
-      break;
-  }
-});
-
-window.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'a':
-      keys.a.pressed = false;
-      break;
-    case 'd':
-      keys.d.pressed = false;
-      break;
-  }
-});
